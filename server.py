600f77f8b579b293fa7bbce7088d401dacb09f16
Anandsingh446 <83154337+Anandsingh446@users.noreply.github.com>
2021-05-23 11:06:28 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 from flask import Flask
 
 PORT = 8000
-MESSAGE = "Hi Anand How are you\n"
+MESSAGE = "x = str("s1") # x will be 's1'
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0'"
 
 app = Flask(__name__)
 
